892a2dfafc67e0d811818ee450e1e67be3f1c483
Mark Domansky <4317290+markdomansky@users.noreply.github.com>
2017-11-25 20:19:11 -0600

    Initial Commit

---
+++ Test-Project/default.aspx.vb
@@ -0,0 +1,8 @@
+﻿Public Class _default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = My.Settings.configfile
+    End Sub
+
+End Class
