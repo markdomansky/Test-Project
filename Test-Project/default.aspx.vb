629634702c412f64edb34879918558f6581119f3
Mark Domansky <4317290+markdomansky@users.noreply.github.com>
2017-11-25 20:21:37 -0600

    changes

--- Test-Project/default.aspx.vb
+++ Test-Project/default.aspx.vb
@@ -3,6 +3,7 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Label1.Text = My.Settings.configfile
+        'change
     End Sub
 
 End Class
